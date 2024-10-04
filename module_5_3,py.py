6e3817f3e22d34dbfc5c5794454e4a9c2d090e02
evgeniilisyanskii <evgeniilisyanskii@yandex.ru>
2024-10-04 15:32:30 +1000

    Add files via upload

---
+++ module_5_3,py.py
@@ -0,0 +1,69 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, Количество этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other. number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other. number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other. number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other. number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other. number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other. number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, other):
+        return self + other
+
+    def __radd__(self, other):
+        return self + other
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)
+
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
